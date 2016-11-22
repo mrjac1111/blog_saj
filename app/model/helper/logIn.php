14c1a7a66c259f963d8057d634e6b72aa43023c6
mrjac1111 <mrjac1111@gmail.com>
2016-11-22 09:32:01 -0800

    Login Compleate

--- app/model/helper/logIn.php
+++ app/model/helper/logIn.php
@@ -13,16 +13,34 @@ class logIn
     function userLogIn($email,$password){
 
         if(!isset($_SESSION["logInSuccessfully"])){
-             $qry = "SELECT id from signup WHERE email='$email' AND  password='$password'";
+             $qry = "SELECT * from signup WHERE email='$email' AND  password='$password'";
                $row= mysqli_query($this->connection,$qry);
-           $user= mysqli_num_rows($row);
-            if($user){
-                print_r($user);
+            print_r($row);
+           $is_user_exist= mysqli_num_rows($row);
+            if($is_user_exist){
+                //print_r($user);
+                $user_data=mysqli_fetch_array($row);
+                 $user_id=$user_data['id'];
+                 $user_first_name=$user_data['firstName'];
+                 $user_last_name=$user_data['lastName'];
+                 $user_profile_image=$user_data['profileImage'];
+                 $user_gender=$user_data['gender'];
+
+                $_SESSION['user_id']=$user_id;
+                $_SESSION['user_first_name']=$user_first_name;
+                $_SESSION['user_last_name']=$user_last_name;
+                $_SESSION['user_profile_image']=$user_profile_image;
+                $_SESSION['user_gender']=$user_gender;
+               echo  $_SESSION['log_in_message']='loginSuccessaly';
+
+
+
+
             }else{
-                echo 'no';
+              echo  $_SESSION['log_in_message']='loginIsNotSuccessaly';
             }
             die;
-            if ($this->connection->query($qry) ==0) {
+            /*if ($this->connection->query($qry) ==0) {
 
 
                 echo $this->logInUpSuccessfully="log In Successfully";
@@ -36,7 +54,7 @@ class logIn
                // echo "Error: " . $qry . "<br>" . $this->connection->error;
 
 
-            }
+            }*/
 
         }
     }
